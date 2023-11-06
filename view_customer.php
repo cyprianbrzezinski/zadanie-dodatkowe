6249d7068a3e3b0ae38369fddf9bf0ac314f1941
Cyprian Brzezinski <cyprian200454@gmail.com@gmail.com>
2023-11-06 13:55:10 +0100

    dodany widok uzytkownikow

---
+++ view_customer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Dodanie uzytkownika</title>
+    <style>
+        .powrot {
+            float: right;
+            margin-right: 20%;
+            background: green;
+            color: red;
+            padding: 20px;
+            text-decoration: none;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+        }
+    </style>
+</head>
+
+<body>
+
+    <a href="index.html" class="powrot">powrot</a>
+    <?php
+    $zmiennaB = mysqli_connect("localhost", "root", "", "erpdatabase");
+    $sprawdzanie = "Select imie, nazwisko, adres, e_mail from customers";
+    $wysylanie = mysqli_query($zmiennaB, $sprawdzanie);
+    echo "<ol>";
+    while ($pobieranie = mysqli_fetch_array($wysylanie)) {
+        echo "<li>";
+        echo $pobieranie["imie"];
+        echo $pobieranie["nazwisko"];
+        echo "</li>";
+    }
+    echo "</ol>"
+    ?>
+
+</body>
+
+</html>
