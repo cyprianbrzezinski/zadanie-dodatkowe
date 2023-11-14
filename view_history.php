5c69eb4f7ed6109d5daec89fd3bd61e58a728c8b
Cyprian Brzezinski <cyprian200454@gmail.com@gmail.com>
2023-11-14 16:45:02 +0100

    edycja tytulow poszczegolnych plikow

--- view_history.php
+++ view_history.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Dodanie uzytkownika</title>
+    <title> Podglad historii</title>
     <style>
         .powrot {
             float: right;
